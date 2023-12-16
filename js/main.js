990f4ea99c568ad345601d3d719ea6a8af038e6c
sawnedcom <101937469+sawnedcom@users.noreply.github.com>
2023-12-16 10:35:17 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function astronotCof() {
+    let astro = document.getElementById("astro")
+    let regular = document.getElementById("regular")
+    let asli = document.getElementById("asli")
+    regular.style.display = "none"
+    astro.style.display = "block"
+    asli.style.display = "none"
+}
+
+function regularCof () {
+    let astro = document.getElementById("astro")
+    let regular = document.getElementById("regular")
+    let asli = document.getElementById("asli")
+    regular.style.display = "block"
+    astro.style.display = "none"
+    asli.style.display = "none"
+}
+
+function asliCof() {
+    let astro = document.getElementById("astro")
+    let regular = document.getElementById("regular")
+    let asli = document.getElementById("asli")
+    regular.style.display = "none"
+    astro.style.display = "none"
+    asli.style.display = "block"
+}
+
